901abfdf0878f133df5ac642509a2e6765230bac
AlfieDarko <alfie.darko1@tesco.com>
2020-01-23 21:47:28 +0000

    added validation and react-hooks-form

---
+++ components/form.jsx
@@ -0,0 +1,143 @@
+import React, { useReducer } from "react";
+import { useForm } from "react-hook-form";
+
+const Form = () => {
+  const { register, handleSubmit, setValue, watch, errors } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  };
+  const [inputValues, setInputValues] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { firstName: "", lastName: "" }
+  );
+
+  const handleOnChange = event => {
+    const { name, value } = event.target;
+    setInputValues({ [name]: value });
+  };
+  return (
+    <form
+      className="lead-gen__form lead-gen--box-shadow"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="lead-gen__input-wrap">
+        <input
+          className={`${
+            errors.firstName
+              ? "lead-gen__input--error-state"
+              : "lead-gen__input"
+          }`}
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          ref={register({
+            required: {
+              value: true,
+              message: "First Name is required"
+            },
+            maxLength: 15,
+            pattern: /^[A-Za-z]+$/i
+          })}
+        />
+        {errors && errors.firstName && (
+          <span className="lead-gen__input--error-icon">⚠️</span>
+        )}
+      </div>
+      <span className="lead-gen__input--error-text">
+        {errors.firstName && errors.firstName.message}
+      </span>
+
+      <div className="lead-gen__input-wrap">
+        <input
+          className={`${
+            errors.lastName ? "lead-gen__input--error-state" : "lead-gen__input"
+          }`}
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          ref={register({
+            required: {
+              value: true,
+              message: "Last Name is required"
+            },
+            maxLength: 15,
+            pattern: /^[A-Za-z]+$/i
+          })}
+        />
+        {errors && errors.lastName && (
+          <span className="lead-gen__input--error-icon">⚠️</span>
+        )}
+      </div>
+      <span className="lead-gen__input--error-text">
+        {errors.lastName && errors.lastName.message}
+      </span>
+
+      <div className="lead-gen__input-wrap">
+        <input
+          className={`${
+            errors.email ? "lead-gen__input--error-state" : "lead-gen__input"
+          }`}
+          type="text"
+          name="email"
+          placeholder="Email Address"
+          ref={register({
+            required: {
+              value: true,
+              message: "Email is required"
+            }
+          })}
+        />
+        {errors && errors.email && (
+          <span className="lead-gen__input--error-icon">⚠️</span>
+        )}
+      </div>
+      <span className="lead-gen__input--error-text">
+        {errors.email && errors.email.message}
+      </span>
+
+      <div className="lead-gen__input-wrap">
+        <input
+          className={`${
+            errors.password ? "lead-gen__input--error-state" : "lead-gen__input"
+          }`}
+          type="text"
+          name="password"
+          placeholder="Password"
+          ref={register({
+            required: {
+              value: true,
+              message: "Password is required"
+            }
+          })}
+        />
+        {errors && errors.password && (
+          <span className="lead-gen__input--error-icon">⚠️</span>
+        )}
+      </div>
+      <span className="lead-gen__input--error-text">
+        {errors.password && errors.password.message}
+      </span>
+
+      {/*  IRL use a button for a11y reasons! */}
+      <div
+        className="lead-gen__btn"
+        role="button"
+        onClick={handleSubmit(onSubmit)}
+        type="submit"
+      >
+        <span className="lead-gen__btn--text-size-16">
+          CLAIM YOUR FREE TRIAL
+        </span>
+      </div>
+
+      <p className="lead-gen__small-print">
+        By clicking the button you are agreeing to our
+        <span className="lead-gen__small-print--important">
+          Terms and Services
+        </span>
+      </p>
+    </form>
+  );
+};
+
+export default Form;
--- components/main.jsx
+++ components/main.jsx
@@ -1,44 +1,28 @@
-import React, { PureComponent } from 'react'
-import 'normalize.css';
-import '../styles/main.scss'
-
+import React, { PureComponent } from "react";
+import "normalize.css";
+import "../styles/main.scss";
+import Form from "./form";
 const Main = () => (
   <div className="container">
     <div className="sales-copy__container">
-      <h1 className="sales-copy__header">
-        Learn to code by watching others
-      </h1>
+      <h1 className="sales-copy__header">Learn to code by watching others</h1>
       <p className="sales-copy__paragraph">
         See how experienced developers solve problems in real-time. <br></br>
         Watching scripted tutorials is great, but understanding how developers
         think is invaluable
       </p>
-    </div> 
+    </div>
 
-      <div className="lead-gen__container">
-        <div className="lead-gen__offer">
-          <p className="lead-gen__offer--font-weight-200">
-            <strong>Try it free 7 days</strong> then $20/mo. thereafter
-          </p>
-        </div>
-
-        <form className="lead-gen__form lead-gen--box-shadow" action="" method="post">
-          <input className="lead-gen__input" type="text" name="firstname" value="First Name"/>
-          <input className="lead-gen__input" type="text" name="lastname" value="Last Name"/>
-          <input className="lead-gen__input" type="text" name="email" value="Email Address"/>
-          <input className="lead-gen__input" type="text" name="password" value="Password"/>
-
-
-          <div className="lead-gen__btn">
-            <span className="lead-gen__btn--text-size-16">CLAIM YOUR FREE TRIAL</span> 
-          </div>
-
-          <p className="lead-gen__small-print">
-              By clicking the button you are agreeing to our <span className="lead-gen__small-print--important">Terms and Services</span>
-          </p>
-        </form>  
+    <div className="lead-gen__container">
+      <div className="lead-gen__offer">
+        <p className="lead-gen__offer--font-weight-200">
+          <strong>Try it free 7 days</strong> then $20/mo. thereafter
+        </p>
       </div>
-  </div>
-)
 
-export default Main
+      <Form />
+    </div>
+  </div>
+);
+
+export default Main;
